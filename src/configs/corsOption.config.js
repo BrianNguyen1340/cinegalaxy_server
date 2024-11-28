29061127d8c4a193f01c3b2fee520a95e09aee92
Brian Nguyen <anhkhongvui0@gmail.com>
2024-11-28 17:09:00 +0700

    update

--- src/configs/corsOption.config.js
+++ src/configs/corsOption.config.js
@@ -3,17 +3,10 @@
  * "I turn lines of code into game-changing solutions!"
  */
 
-import { varEnv } from './variableEnv.config'
-
-// import { WHITELIST_DOMAINS } from '~/utils/domains'
 export const WHITELIST_DOMAINS = ['http://localhost:3000']
 
 export const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin && varEnv.NODE_ENV === 'devlopment') {
-      return callback(null, true)
-    }
-
     if (WHITELIST_DOMAINS.includes(origin)) {
       callback(null, true)
     }
