d876e6ff282b74c33b4138d0028b04d65566563a
Brian Nguyen <anhkhongvui0@gmail.com>
2024-11-28 17:04:05 +0700

    update

--- src/configs/corsOption.config.js
+++ src/configs/corsOption.config.js
@@ -3,15 +3,22 @@
  * "I turn lines of code into game-changing solutions!"
  */
 
-import { WHITELIST_DOMAINS } from '~/utils/domains'
+import { varEnv } from './variableEnv.config'
+
+// import { WHITELIST_DOMAINS } from '~/utils/domains'
+export const WHITELIST_DOMAINS = ['http://localhost:3000']
 
 export const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || WHITELIST_DOMAINS.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error(`${origin} not allowed by our CORS policy!`))
+    if (!origin && varEnv.NODE_ENV === 'devlopment') {
+      return callback(null, true)
     }
+
+    if (WHITELIST_DOMAINS.includes(origin)) {
+      callback(null, true)
+    }
+
+    return callback(new Error(`${origin} not allowed by our CORS policy!`))
   },
   credentials: true,
   optionSuccessStatus: 200,
